26554a7ad5b22e4cac3a3195531ef5a019c7c8cf
OriYosefiMSFT <oriy@microsoft.com>
2016-08-23 23:54:50 +0300

    make default only update default branch. expose 8080

--- docker/startup/default.sh
+++ docker/startup/default.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-if [$# -eq 1]
-    git checkout $1
-    git pull
-    gradlew assemble war 
-fi
-cp /ApplicationInsights-Java/test/webapps/bookstore-spring/build/libs/bookstore-spring.war /tomcat/apache-tomcat-$TOMCAT_VERSION/webapps/
+git pull
